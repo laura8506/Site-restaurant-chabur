d1d8aa10471a7e832a98571d7d124c045424ddb7
laura8506 <91312401+laura8506@users.noreply.github.com>
2021-10-13 16:31:01 +0200

    responsive pages contact et nous_trouver

--- contact.php
+++ contact.php
@@ -7,19 +7,19 @@
     <form action="" method="post">
         <div>
             <label for="name">Votre nom (obligatoire)</label><br>
-            <input class="formulaire_contact" id="name" type="text" name="nom" style="width:750px">
+            <input class="formulaire_contact" id="name" type="text" name="nom">
         </div><br>
         <div>
             <label for="email">Votre adresse email (obligatoire)</label><br>
-            <input class="formulaire_contact" id="email" type="email" name="email" style="width:750px">
+            <input class="formulaire_contact" id="email" type="email" name="email">
         </div><br>
         <div>
             <label for="object">Objet</label><br>
-            <input class="formulaire_contact" id="object" type="text" name="objet" style="width:750px">
+            <input class="formulaire_contact" id="object" type="text" name="objet">
         </div><br>
         <div>
             <label for="message">Votre message</label><br>
-            <textarea class="formulaire_contact" id="message" name="message" style="width:750px"></textarea>
+            <textarea class="formulaire_contact" id="message" name="message"></textarea>
         </div><br>
         <button type="submit" class="bouton_contact"><a href="#">Envoyer</a></button><br><br>
     </form>
